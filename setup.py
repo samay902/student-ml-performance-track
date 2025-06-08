ef20d2cbc4e00c6c7dfb47f64b1cd0524a381d71
samay902 <samaybasnet@gmail.com>
2025-06-08 08:56:16 +0545

    added setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from  setuptools import find_packages,setup
+from typing import List
+hypen_dot_e="-e ."
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if hypen_dot_e in requirements:
+            requirements.remove(hypen_dot_e)
+    return requirements
+
+
+
+setup(
+name="student-ml-performance-track",
+version='0.0.1',
+author="Samay",
+author_email="samaybasnet@gmail.com",
+packages=find_packages(),
+install_requires=get_requirements("requirements.txt")
+
+
+)
