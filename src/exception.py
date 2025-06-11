07d0c0834224d4291df17fdeaaafafe9ad8afe4c
samay902 <samaybasnet@gmail.com>
2025-06-11 09:22:17 +0545

    update code

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info
     file_name=exc_tb.tb_frame.f_code.co_filename#access the file name
@@ -8,8 +9,14 @@ def error_message_detail(error,error_detail:sys):
     
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):#get all the vars u need for the class
-        super.__init__(error_message)#inherit the error_message
+        super().__init__(error_message)#inherit the error_message
         self.error_message=error_message_detail(error_message,error_detail=error_detail)#make the error message by caling prev func
     def __str__(self):#new func with all the vars within class
         return self.error_message#finally spit out the error message
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:    
     
+        logging.info("divide by zero")
+        raise CustomException(e,sys)
--- src/logger.py
+++ src/logger.py
@@ -12,10 +12,3 @@ logging.basicConfig(
 )
 
 
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:    
-    
-        logging.info("divide by zero")
-        raise CustomException(e,sys):
